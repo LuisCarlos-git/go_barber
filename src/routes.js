19ce6707f912322a43bcb5ebf33d4fb8a5cad88e
luis carlos <luisestrela.bol951@gmail.com>
2020-03-13 23:34:49 -0300

    added config the sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from 'express';
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
+routes.get('/', (req, res) => {
   return res.json({ okay: true });
 });
 
-module.exports = routes;
+export default routes;
