e336ca6959ee7a58b94818a2357ab2abb07a3ce7
luis carlos <luisestrela.bol951@gmail.com>
2020-03-14 11:05:16 -0300

    created model users

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/', (req, res) => res.json({ okay: true }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luis Carlos',
+    email: 'luiscarlos@gmail.com',
+    password_hash: '1234',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
