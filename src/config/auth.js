b61825ebe47bf01576e094cf8e653e5191537802
luis carlos <luisestrela.bol951@gmail.com>
2020-03-15 11:23:19 -0300

    user update

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '6e2362890c0d0fbbaf34b3b975f97d36',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/Usercontroller';
+import SessionControler from './app/controllers/SessionControler';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
